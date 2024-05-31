84bb33aabaa1300989f3083666291ac510d1b37a
Lon60 <leon.liechti@icloud.com>
2024-05-31 22:21:27 +0200

    init

---
+++ api/src/main/java/com/faithsafe/api/FaithSafeWebApi.java
@@ -0,0 +1,12 @@
+package com.faithsafe.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaithSafeWebApi {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FaithSafeWebApi.class, args);
+  }
+}
