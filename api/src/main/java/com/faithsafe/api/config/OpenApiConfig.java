1308cf63b6c522c529748d695a56a42659b5a87a
Lon60 <leon.liechti@icloud.com>
2024-06-06 21:30:39 +0200

    hotfix

--- api/src/main/java/com/faithsafe/api/config/OpenApiConfig.java
+++ api/src/main/java/com/faithsafe/api/config/OpenApiConfig.java
@@ -2,12 +2,23 @@ package com.faithsafe.api.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @OpenAPIDefinition(
+    servers = {
+        @Server(
+            description = "Local server",
+            url = "http://localhost:8080/"
+        ),
+        @Server(
+            description = "Production server",
+            url = "https://api.faithsafe.net/"
+        )
+    },
     security = {
         @SecurityRequirement(name = "bearerAuth")
     }
